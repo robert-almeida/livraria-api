abd6779fc73aa5a303bbd3acf63f138e9807fbea
robert-almeida <robertwagner.almeida@hotmail.com>
2021-09-29 22:48:11 -0300

    mapeamento das entidades Autor e Livro

--- src/main/java/br/com/alura/livraria/modelo/Autor.java
+++ src/main/java/br/com/alura/livraria/modelo/Autor.java
@@ -2,6 +2,12 @@ package br.com.alura.livraria.modelo;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +19,12 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="autores")
 public class Autor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
 	private String nome;
 	private String email;
 	private LocalDate dataNascimento;
--- src/main/java/br/com/alura/livraria/modelo/Livro.java
+++ src/main/java/br/com/alura/livraria/modelo/Livro.java
@@ -2,6 +2,13 @@ package br.com.alura.livraria.modelo;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,9 +20,16 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="livros")
 public class Livro {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
 	private String titulo;
 	private LocalDate dataLancamento;
-	private int numeroPaginas;
+	private Integer numeroPaginas;
+	
+	@ManyToOne
 	private Autor autor;
 }
