f32a89cc0ce5cc6744dfeb383851d5999f56669e
robert-almeida <robertwagner.almeida@hotmail.com>
2021-11-04 00:07:36 -0300

    Validacao do token via filter nas requisicoes

--- src/main/java/br/com/alura/livraria/infra/security/TokenService.java
+++ src/main/java/br/com/alura/livraria/infra/security/TokenService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.livraria.modelo.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -18,10 +19,31 @@ public class TokenService {
 		Usuario logado = (Usuario) authentication.getPrincipal();
 		return Jwts
 				.builder()
-				.setId(logado.getId().toString())
+				.setSubject(logado.getId().toString())
 				.signWith(SignatureAlgorithm.HS256, secretKey)
-				.compact()
-				;
+				.compact();
+	}
+	
+	public boolean isValido(String token) {
+		try {
+			Jwts
+			.parser()
+			.setSigningKey(secretKey)
+			.parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public Long extrairIdUsuario(String token) {
+			Claims claims = Jwts
+			.parser()
+			.setSigningKey(secretKey)
+			.parseClaimsJws(token)
+			.getBody();
+		return Long.parseLong(claims.getSubject());
+
 	}
 
 }
